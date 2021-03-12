7e46cdd510aea7a1736aa0fbd8a266a45d216062
Michael Redig <mredig@gmail.com>
2021-03-12 12:44:13 -0600

    (fix) works

--- Demo/Image Zoom/RootViewController.swift
+++ Demo/Image Zoom/RootViewController.swift
@@ -15,7 +15,13 @@ class RootViewController: UIViewController {
 		let zoomyVC = ZoomyViewController(zoomedView: imageView)
 
 		view.addSubview(zoomyVC.view)
-//		view.constrain(subview: zoomyVC.view)
+		zoomyVC.view.translatesAutoresizingMaskIntoConstraints = false
+		NSLayoutConstraint.activate([
+			view.leadingAnchor.constraint(equalTo: zoomyVC.view.leadingAnchor),
+			view.trailingAnchor.constraint(equalTo: zoomyVC.view.trailingAnchor),
+			view.bottomAnchor.constraint(equalTo: zoomyVC.view.bottomAnchor),
+			view.topAnchor.constraint(equalTo: zoomyVC.view.topAnchor),
+		])
 		addChild(zoomyVC)
 		zoomyVC.didMove(toParent: self)
 	}

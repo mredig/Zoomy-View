75a9bc93428b077b172902ae2fce425e95e7080d
Michael Redig <mredig@gmail.com>
2021-03-11 18:21:45 -0600

    (feat) well behaved zooming and scrolling

--- Image Zoom/ViewController.swift
+++ Image Zoom/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 	var lastSize: CGSize = .zero
 	let scrollView = UIScrollView()
 	let imageView = UIImageView()
+	var image: UIImage?
 
 	override func loadView() {
 		self.view = scrollView
@@ -20,35 +21,57 @@ class ViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
-//		let imageURL = Bundle.main.url(forResource: "zhara", withExtension: "jpg")!
-//		let data = try! Data(contentsOf: imageURL)
-		let image = UIImage(named: "zhara")
+		image = UIImage(named: "zhara")
 		imageView.image = image
-		imageView.contentMode = .center
 
-		imageView.frame = CGRect(origin: .zero, size: image?.size ?? .zero)
-		imageView.backgroundColor = .systemBlue
-
-		imageView.translatesAutoresizingMaskIntoConstraints = false
 		scrollView.addSubview(imageView)
-		scrollView.constrain(subview: imageView)
-		
+
 		scrollView.backgroundColor = .systemPink
-		scrollView.maximumZoomScale = 10
-		scrollView.minimumZoomScale = 1
+		scrollView.maximumZoomScale = 3
 		scrollView.delegate = self
 
+	}
 
-		DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-			print(self)
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+
+		if let image = image {
+			setupMinZoom(for: image.size)
 		}
 	}
 
-	override func viewDidLayoutSubviews() {
-		super.viewDidLayoutSubviews()
-		guard scrollView.bounds.size != lastSize else { return }
-		defer { lastSize = scrollView.bounds.size }
-		imageView.center = scrollView.center
+	override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+		// detect device rotation
+		super.traitCollectionDidChange(previousTraitCollection)
+
+		if let image = image {
+			setupMinZoom(for: image.size)
+		}
+	}
+
+	private func setupMinZoom(for imageSize: CGSize) {
+		let minWidthScale = view.bounds.width / imageSize.width
+		let minHeightScale = view.bounds.height / imageSize.height
+		let minScale = min(minWidthScale, minHeightScale)
+
+		scrollView.minimumZoomScale = minScale
+		scrollView.zoomScale = minScale
+
+		let width = imageSize.width * minScale
+		let height = imageSize.height * minScale
+		let newImageFrame = CGRect(origin: .zero, size: CGSize(width: width, height: height))
+		imageView.frame = newImageFrame
+
+		centerImage()
+	}
+
+	private func centerImage() {
+		let imageViewSize = imageView.frame.size
+		let scrollViewSize = view.frame.size
+		let vertPadding = max((scrollViewSize.height - imageViewSize.height) / 2, 0)
+		let horizPadding = max((scrollViewSize.width - imageViewSize.width) / 2, 0)
+
+		scrollView.contentInset = UIEdgeInsets(horizontal: horizPadding, vertical: vertPadding)
 	}
 }
 
@@ -56,6 +79,10 @@ class ViewController: UIViewController {
 extension ViewController: UIScrollViewDelegate {
 	func viewForZooming(in scrollView: UIScrollView) -> UIView? {
 		imageView
+	}
+
+	func scrollViewDidZoom(_ scrollView: UIScrollView) {
+		centerImage()
 	}
 }
 
